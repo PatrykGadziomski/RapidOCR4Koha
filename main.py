273b3fccaa03888ef710f41bac7863f1ea2fd2d5
PatrykGadziomski <gadziomski@etik.com>
2026-07-30 14:56:50 +0200

    add marcxml schema

--- main.py
+++ main.py
@@ -1,24 +1,70 @@
 # https://github.com/RapidAI/RapidOCR
 
-# from rapidocr import RapidOCR
+from rapidocr import RapidOCR
+import re
 
 # --- Konfiguration ---
-# IMAGE_PATH = "imgs/Adobe Scan 24 Jul 2026.jpg"   # Pfad zu deinem Bild anpassen
-# OUTPUT_PATH = "output/output.txt"                # Ausgabedatei
-# INDENT_THRESHOLD = 20                     # Pixel, ggf. anpassen
+IMAGE_PATH = "data/20260730110422904.jpg"
+OUTPUT_PATH = "data/output.txt"
+VIZ_PATH = "viz/output.jpg"
+
+keyord_schema_gnd_dnb = """
+<datafield tag="650" ind1=" " ind2="7">
+    <subfield code="0">(DE-588){gnd_code}</subfield>
+    <subfield code="0">https://d-nb.info/gnd/{gnd_code}</subfield>
+    <subfield code="0">(DE-101){dnb_code}</subfield>
+    <subfield code="a">{keyword}</subfield>
+    <subfield code="9">rswk-swf</subfield>
+    <subfield code="2">gnd</subfield>
+  </datafield>
+"""
+
+keyord_schema = """
+<datafield tag="650" ind1=" " ind2="7">
+    <subfield code="a">{keyword}</subfield>
+  </datafield>
+"""
 
 # --- OCR ausführen ---
-# engine = RapidOCR()
+engine = RapidOCR(
+    params={
+        "Rec.lang_type": "german",
+        "Det.lang_type": "german",
+    }
+)
 
-# engine = RapidOCR(
-#     params={
-#         "Rec.lang_type": "german",
-#         "Det.lang_type": "german",
-#     }
-# )
+result = engine(IMAGE_PATH)
+print(result)
+# result.vis("vis_result.jpg")
 
-# result = engine(IMAGE_PATH)
-# print(engine.params)
+# Cleaning
+# - remove charcters after the number
+# - remove special characters
+# - remove sub categories? Can I sort them in some way?
+# TODO: Remove blank strings
+result = sorted(
+    [''.join([i for i in text if not i.isdigit()]) for text in result.txts]
+    )
+
+result = [i.strip() for i in result if i != ""]
+
+for i in result:
+    print(i)
+    # print(keyord_schema_gnd_dnb.format(gnd_code=0, dnb_code=0, keyword=i)) # Test
+    # break
+
+
+# - Connect to GND and DNB via API
+# - save kexwords in MarcXML format, if possible
+
+
+
+
+
+# ---------------------------------
+# For literature source recognition
+# ---------------------------------
+
 
 # lines = []
 # for box, text, score in zip(result.boxes, result.txts, result.scores):
@@ -58,23 +104,3 @@
 #         f.write(f"[{i}] {entry}\n\n")
 
 # print(f"{len(entries)} Quellen erkannt. Gespeichert in: {OUTPUT_PATH}")
-
-
-
-
-# Test
-
-from rapidocr import RapidOCR
-
-engine = RapidOCR(
-    params={
-        "Rec.lang_type": "german",
-        "Det.lang_type": "german",
-    }
-)
-
-img_url = "20260730110422904.jpg"
-result = engine(img_url)
-print(result)
-
-result.vis("vis_result.jpg")
